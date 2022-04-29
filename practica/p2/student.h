2743f2af22a3571f292a5e06f25d44c945a88ec1
GalileoCap <galileocapp@gmail.com>
2022-04-29 00:17:36 -0300

    CLEANING

--- practica/p2/student.h
+++ practica/p2/student.h
@@ -14,8 +14,10 @@ extern stack_t *stack;
 typedef struct student
 {
     char name[NAME_LEN]; //NAME_LEN * 1 bytes = 21 bytes
+    // 11-Bytes de padding
     uint32_t dni; //4 bytes
     uint8_t califications[NUM_CALIFICATIONS]; //NUM_CALIFICATIONS * 1 bytes = 3 bytes
+    // 1-Byte de padding
     int16_t concept; //2 bytes
 } student_t;
 
